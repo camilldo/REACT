594e8ac7b2b9eeec7614f312167ba319c1aa1afb
cousseth <thomas.cousseau@reseau.eseo.fr>
2023-01-09 08:29:13 +0100

    First commit

---
+++ projet/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Toolbar from './Toolbar';
+
+const App = () => {
+    return (
+        <div className="app">
+            <Toolbar />
+        </div>
+    );
+}
+
+export default App;
