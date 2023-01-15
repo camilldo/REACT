eadff3a66c04132cb144bcb807cc7096825246d8
Dorian <dorian.camilleri@reseau.eseo.fr>
2023-01-15 10:51:55 +0100

    Ajout de la page App

--- projet/components/App.js
+++ projet/components/App.js
@@ -1,11 +1,77 @@
+// import React from 'react';
+// import Toolbar from './Toolbar';
+//
+// const App = () => {
+//     return (
+//         <div className="app">
+//             <Toolbar />
+//         </div>
+//     );
+// }
+//
+// export default App;
+
 import React from 'react';
 import Toolbar from './Toolbar';
+import Link from "next/link";
+import {Card, Col, Layout, Row} from 'antd';
 
 const App = () => {
     return (
-        <div className="app">
-            <Toolbar />
-        </div>
+        <Layout>
+            <div className="app" style={{overflowX:"hidden", minHeight:"100vh", maxHeight:"100%",backgroundSize: "cover", backgroundImage: "url('./assets/background.jpg')",backgroundRepeat:"no-repeat"}}>
+                <Toolbar/>
+                <div>
+                    <div style={{textAlign: 'center', marginTop: '50px'}}>
+                        <p style={{color: 'white', fontSize: '24px'}}>Welcome to the Space Of the Day page! Here you can explore
+                            the latest and greatest in space exploration and technology.</p>
+                    </div>
+                    <div className="page-links">
+                        <Row style={{padding: 32}} gutter={16} justify="center" align="middle">
+                            <Col
+                                xs={24}
+                                sm={12}
+                                md={8}
+                                lg={6}
+                                style={{marginBottom: 16}}
+                            >
+                                <Link href="/">
+                                    <Card hoverable cover={<img alt="Page 1" src="./assets/imageTest.jpg"/>}>
+                                        <Card.Meta style={{textAlign: "center"}} title="Image du Jour"/>
+                                    </Card>
+                                </Link>
+                            </Col>
+                            <Col
+                                xs={24}
+                                sm={12}
+                                md={8}
+                                lg={6}
+                                style={{marginBottom: 16}}
+                            >
+                                <Link href="/">
+                                    <Card hoverable cover={<img alt="Page 2" src="./assets/imageTest.jpg"/>}>
+                                        <Card.Meta style={{textAlign: "center"}} title="Images Aléatoires"/>
+                                    </Card>
+                                </Link>
+                            </Col>
+                            <Col
+                                xs={24}
+                                sm={12}
+                                md={8}
+                                lg={6}
+                                style={{marginBottom: 16}}
+                            >
+                                <Link href="/">
+                                    <Card hoverable cover={<img alt="Page 3" src="./assets/imageTest.jpg"/>}>
+                                        <Card.Meta style={{textAlign: "center"}} title="Images Favorites"/>
+                                    </Card>
+                                </Link>
+                            </Col>
+                        </Row>
+                    </div>
+                </div>
+            </div>
+        </Layout>
     );
 }
 
