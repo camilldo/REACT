d8974f07f003516604f9ab10ab117ceb3b7e5afe
Dorian <dorian.camilleri@reseau.eseo.fr>
2023-01-24 14:57:12 +0100

    hosting firebase

--- projet/next.config.js
+++ projet/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  // reactStrictMode: true,
+  // swcMinify: true,
+  reactStrictMode: false,
+  distDir: "./build",
+  outDir: "./out"
 }
 
 module.exports = nextConfig
